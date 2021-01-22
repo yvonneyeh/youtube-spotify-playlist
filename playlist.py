691a8a91081873351bfd5bf62560277d2fa566af
Yvonne Yeh <yvonneyeh@me.com>
2021-01-22 22:31:31 +0000

    Define Spotify URI search method

--- playlist.py
+++ playlist.py
@@ -7,13 +7,24 @@
 # Add song to new Spotify playlist
 
 import json
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 import requests
-from secrets import spotify_user_id
+import youtube_dl
+
+from exceptions import ResponseException
+from secrets import spotify_token, spotify_user_id
+
 
 class Playlist:
 
     def __self__(self):
         self.user_id = spotify_user_id
+        self.youtube_client = self.get_youtube_client()
+        self.all_song_info = {}
 
     def get_youtube_client(self):
         """Log in to YouTube; return something."""
@@ -43,9 +54,22 @@ class Playlist:
         # playlist id
         return response_json["id"]
 
-    def get_spotify_uri(self):
+    def get_spotify_uri(self, song_name, artist):
         """Search for the song on Spotify."""
-        pass
+
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(song_name,
+                        artist)
+        response = requests.get(query,
+                        headers={
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer {}".format(spotify_token)})
+        response_json = response.json()
+        songs = response_json["tracks"]["items"]
+
+        # only use the first song
+        uri = songs[0]["uri"]
+
+        return uri
 
     def add_song(self):
         """Add a song to Spotify playlist."""
