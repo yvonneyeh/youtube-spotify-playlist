a09afec73762d190d4e4502ccd7fd6479ddfe617
Yvonne Yeh <yvonneyeh@me.com>
2021-01-22 20:49:26 +0000

    Initial commit

---
+++ playlist.py
@@ -0,0 +1,32 @@
+"""A program to generate Spotify playlist that adds songs from your liked videos on YouTube."""
+
+# Log into YouTube
+# Fetch liked YouTube videos
+# Create new Spotify playlist
+# Search for song on Spotify
+# Add song to new Spotify playlist
+
+class Playlist:
+
+    def __self__(self):
+        pass
+
+    def get_youtube_client(self):
+        """Log in to YouTube; return something."""
+        pass
+
+    def get_liked_vids(self):
+        """Get liked videos from YouTube."""
+        pass
+
+    def create_spotify_playlist(self):
+        """Create a new Spotify playlist."""
+        pass
+
+    def get_spotify_uri(self):
+        """Search for the song on Spotify."""
+        pass
+
+    def add_song(self):
+        """Add a song to Spotify playlist."""
+        pass
